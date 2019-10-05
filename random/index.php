edd48cef2681e533874d12c9aa76ee4c3d61d01f
Lounode <lounode@qq.com>
2019-10-05 10:57:11 +0800

    Site updated: 2019-10-05 10:57:11

---
+++ random/index.php
@@ -0,0 +1,13 @@
+<?
+$handle = opendir('/images'); //当前目录
+while (false !== ($file = readdir($handle))) { //遍历该php教程文件所在目录
+list($filesname,$kzm)=explode(".",$file);//获取扩展名
+if ($kzm=="gif" or $kzm=="jpg") { //文件过滤
+if (!is_dir('./'.$file)) { //文件夹过滤
+$array[]=$file;//把符合条件的文件名存入数组
+}
+}
+}
+$suiji=array_rand($array); //使用array_rand函数从数组中随机抽出一个单元
+?>
+<img src="<?=$array[$suiji]?>">
